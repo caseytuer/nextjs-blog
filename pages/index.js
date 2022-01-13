511a801714b2bacf83277c2052b493b89b9f1dcd
Casey Tuer <caseytuer@gmail.com>
2022-01-12 20:24:51 -0800

    deploying

--- pages/index.js
+++ pages/index.js
@@ -9,10 +9,10 @@ export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
-        <title>{siteTitle}</title>
+        <title>{"Casey's World"}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
+        <p>It's ya boi casey</p>
         <p>
           (This is a sample website - you’ll be building a site like this in{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
